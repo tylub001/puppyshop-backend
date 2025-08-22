f9d37e3928a652cb6ce05189ad7f3b7b59c9dd91
Brittany Tylutke <tylub001@gmail.com>
2025-08-22 16:04:57 -0400

    Update root route message

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("🐾 PuppyShop backend is live!");
+  res.send("🐾 PuppyShop backend is live!!!! Hurray!!!");
 });
 
 const PORT = process.env.PORT || 5000;
